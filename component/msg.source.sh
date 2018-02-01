d47536e57938cd28315e0c579f65c2b3fd309040
Rich Moyse <rich@moyse.us>
2018-02-01 11:21:41 -0500

    preserve caller-s return code in all cases.  if not an error when calling error or fatal set return to 1.  Include entire call stack.  fix call level parameter.

--- component/msg.source.sh
+++ component/msg.source.sh
@@ -1,42 +1,91 @@
 #!/bin/bash
+###############################################################################
+#
+#	Purpose:
+#		Provide consistent logging interface & behavior.
+#
+#	Notes:
+#		>	Important conventions when modifying this file:
+#			https://github.com/WhisperingChaos/SOLID_Bash/blob/master/README.md#source-file-conventions
+#
+###############################################################################
 
-msg_VERBOSE_OUTPUT='false'
-msg_INFORM_FILE='&1'
+msg_VERBOSE_OUTPUT='false'; 	# X-cutting concern - msg_inform and _error don't include call stack info with message text - the others do. 
+msg_INFORM_FILE='&1';			# X-cutting concern - define default to appropriate STD??? handle.  Can also redirect to file. Ex: '>/var/log/logging' which will append messages to logging file.
 msg_ERROR_FILE='&2'
 msg_DEBUG_FILE='&2'
-msg_FATAL_FILE='&s2'
+msg_FATAL_FILE='&2'
 
 msg_inform(){
-	msg__basic "$1" 'Inform' "$msg_INFORM_FILE" '2'
+	local -ri rtnCd=$?
+	local -r msg="$1"
+
+	msg__basic "$msg" 'Inform' "$msg_INFORM_FILE" '1'
+
+	return $rtnCd
 }
 
 msg_error(){
-	msg__basic "$1"  'Error'  "$msg_ERROR_FILE" '2'
-	return 1
+	local -i rtnCd=$?
+	local -r msg="$1"
+
+	if [ $rtnCd -eq 0 ]; then rtnCd=1; fi
+	local -r rtnCd	
+
+	msg__basic "$msg"  'Error'  "$msg_ERROR_FILE" '1'
+
+	return $rtnCd
 }
 
 msg_debug() {
+	local -i rtnCd=$?
+	local -r msg="$1"
+	local -ir rtnCdArg=$2
+
+	msg__rtncd_error_chooser "$rtnCdArg" "$rtnCd"
+	local -r rtnCd=$?
 
 	local -r msg_VERBOSE_OUTPUT='true'
-	msg__basic "$1" 'Debug' "$msg_DEBUG_FILE" '2'
+	msg__basic "$msg" 'Debug' "$msg_DEBUG_FILE" '1'
+	
+	return $rtnCd
 }
 
 msg_fatal(){
-	
-	local -r msg_VERBOSE_OUTPUT='true'
-	local -i clvl=2
+	local -i rtnCd=$?
+	local -r msg="$1"
+	local -ir rtnCdArg=$2
 
-	msg__basic "$1" 'Fatal' "$msg_FATAL_FILE" $clvl
+	msg__rtncd_error_chooser "$rtnCdArg" "$rtnCd"
+	local -r rtnCd=$?	
+
+	local -r msg_VERBOSE_OUTPUT='true'
+	local -i clvl=1
+
+	msg__basic "$msg" 'Fatal' "$msg_FATAL_FILE" $clvl
 	# print call frames for functions above the one that called fatal
 	# This if frame 0 then frame 1 issued call. Report on remaining
 	# call stack: frame 2,3,4,...,n
 	local   pasCallFrame
-	for (( clvl; true; clvl++)) {
+	local -ri maxCallDepth=${#BASH_LINENO[@]}-1
+	for (( clvl; clvl < maxCallDepth; clvl++ )) {
 		if [ -z "${FUNCNAME[$clvl]}" ]; then break; fi
 		msg__call_frame "$clvl" 'pasCallFrame'
-		eval echo \"Caller\:\ \$pasCallFrame\" \>$msg_FATAL_FILE
+		#eval echo \" +\ Caller\:\$pasCallFrame\" \>$msg_FATAL_FILE
+		eval echo \"\ \+\ \ Caller\:\$pasCallFrame\" \>$msg_FATAL_FILE
+
 	}
-	exit 1
+	exit $rtnCd
+}
+
+msg__rtncd_error_chooser(){
+	local -ir asArg=$1
+	local -ir callers=$2
+
+	# prededence: argument from caller, caller's environment then 1
+	if [ -n "$asArg" ]   && [ $asArg -ne 0 ];  then return $asArg;   fi
+	if [ -n "$callers" ] && [ $callers -ne 0 ]; then return $callers; fi
+	return 1
 }
 
 msg__basic(){
@@ -47,20 +96,21 @@ msg__basic(){
 
 	if [ -z "$msg" ]; then msg="Message not specified."; fi
 
-	local verbose
+	local pasVerbose
 	if [ "$msg_VERBOSE_OUTPUT" == 'true' ]; then
-		msg__call_frame "$msgCallerFrame" 'verbose'
+		msg__call_frame "$msgCallerFrame" 'pasVerbose'
 	fi
-	eval echo \"msgType=\'\$msgType\'\" \"msg\=\'\$msg\'\"\$verbose \>$outputFile
+	local -r msgQuotEscape="${msg/\'/\'\\\'\'}"	
+	eval echo \"msgType=\'\$msgType\'\" \"msg\=\'\$msgQuotEscape\'\"\$pasVerbose \>$outputFile
 }
 
 msg__call_frame(){
 	local -ri callLvl=$1
 	local -r rtnCallFrame="$2"
 
-	local  callFrame=" file='${BASH_SOURCE[$callLvl+1]}'"
-	callFrame="$callFrame lineNo='${BASH_LINENO[$callLvl+1]}'"
-	callFrame="$callFrame func='${FUNCNAME[$callLvl+1]}'"
+	local  callFrame=" file='${BASH_SOURCE[$callLvl+2]}'"
+	callFrame="$callFrame lineNo=${BASH_LINENO[$callLvl+1]}"
+	callFrame="$callFrame func='${FUNCNAME[$callLvl+2]}'"
 	eval $rtnCallFrame\=\"\$callFrame\"
 }
 ###############################################################################
